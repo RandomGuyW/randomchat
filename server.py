92d01026fe5e181b4ab755dad4b441a45bac134b
RandomGuyW <99072908+RandomGuyW@users.noreply.github.com>
2022-02-05 10:27:50 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,49 +1,62 @@
-import threading
+import socket,select
 import socket
-host = '127.0.0.1'
-port = 59000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
+import threading
 
+class Server:
+    def __init__(self):
+        self.start_server()
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+    def start_server(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        
+        host = socket.gethostbyname(socket.gethostname())
+        port = int(input('Enter port to run the server on --> '))
 
+        self.clients = []
 
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} odešel!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
+        self.s.bind((host,port))
+        self.s.listen(100)
+    
+        print('Running on host: '+str(host))
+        print('Running on port: '+str(port))
 
+        self.username_lookup = {}
 
-def receive():
-    while True:
-        print('Čekám na spojení')
-        client, address = server.accept()
-        print(f'spojení s dokončeno {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} přišel do chat roomky'.encode('utf-8'))
-        client.send('připojeno'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
+        while True:
+            c, addr = self.s.accept()
 
+            username = c.recv(1024).decode()
+            
+            print('New connection. Username: '+str(username))
+            self.broadcast('New person joined the room. Username: '+username)
 
-if __name__ == "__main__":
-    receive()
+            self.username_lookup[c] = username
+
+            self.clients.append(c)
+             
+            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+
+    def broadcast(self,msg):
+        for connection in self.clients:
+            connection.send(msg.encode())
+
+    def handle_client(self,c,addr):
+        while True:
+            try:
+                msg = c.recv(1024)
+            except:
+                c.shutdown(socket.SHUT_RDWR)
+                self.clients.remove(c)
+                
+                print(str(self.username_lookup[c])+' left the room.')
+                self.broadcast(str(self.username_lookup[c])+' has left the room.')
+
+                break
+
+            if msg.decode() != '':
+                print('New message: '+str(msg.decode()))
+                for connection in self.clients:
+                    if connection != c:
+                        connection.send(msg)
+
+server = Server()
